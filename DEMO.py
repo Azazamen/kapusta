c70161ab2dac43c8a0018bd572363a380d8e35e8
Postolow Alex <sascha.postoloff@yandex.ru>
2025-02-19 14:52:54 +0300

    Создание индекса

--- DEMO.py
+++ DEMO.py
@@ -3,15 +3,8 @@ import sqlite3
 #Создаём подключение к бд (создастся)
 connection = sqlite3.connect ('my_db.db')
 cursor = connection.cursor()
-#Созд табл
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS Users (
-id INTEGER PRIMARY KEY,
-username TEXT NOT NULL,
-email TEXT NOT NULL,
-age INTEGER
-)
-''')
+#Созд индекса emal
+cursor.execute('CREATE INDEX idx_email ON Users (email)')
 #сохр
 connection.commit()
 connection.close()
