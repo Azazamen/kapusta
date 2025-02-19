717dc2a819b14afc1392909889147883955c6d62
Postolow Alex <sascha.postoloff@yandex.ru>
2025-02-19 14:55:45 +0300

    Добавление данных

--- DEMO.py
+++ DEMO.py
@@ -3,8 +3,10 @@ import sqlite3
 #Создаём подключение к бд (создастся)
 connection = sqlite3.connect ('my_db.db')
 cursor = connection.cursor()
-#Созд индекса emal
-cursor.execute('CREATE INDEX idx_email ON Users (email)')
+
+#добавление нового польз
+cursor.execute('INSERT INTO Users (username, email, age) VALUES (?, ?, ?)', ('newuser', 'newuser@example.com', 28))
+
 #сохр
 connection.commit()
 connection.close()
