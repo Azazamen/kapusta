07aaada2ae8339a7349e2cd30871ab117db153b8
Postolow Alex <sascha.postoloff@yandex.ru>
2025-02-19 14:57:13 +0300

    Изменение данных

--- DEMO.py
+++ DEMO.py
@@ -4,8 +4,8 @@ import sqlite3
 connection = sqlite3.connect ('my_db.db')
 cursor = connection.cursor()
 
-#добавление нового польз
-cursor.execute('INSERT INTO Users (username, email, age) VALUES (?, ?, ?)', ('newuser', 'newuser@example.com', 28))
+# Изменение данных
+cursor.execute('UPDATE Users SET age = ? WHERE username = ?', (29, 'newuser'))
 
 #сохр
 connection.commit()
