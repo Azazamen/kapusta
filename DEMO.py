2e34f7ed4a2a315f52f54f812353ec8b93854a63
Postolow Alex <sascha.postoloff@yandex.ru>
2025-02-19 14:51:22 +0300

    Изменение кода + доб таблицы + доб курсора

--- DEMO.py
+++ DEMO.py
@@ -2,5 +2,16 @@ import sqlite3
 
 #Создаём подключение к бд (создастся)
 connection = sqlite3.connect ('my_db.db')
-
+cursor = connection.cursor()
+#Созд табл
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users (
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER
+)
+''')
+#сохр
+connection.commit()
 connection.close()
