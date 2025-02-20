7142b1b98ecfac20c636acf08d4e7c00e3cb0378
Postolow Alex <sascha.postoloff@yandex.ru>
2025-02-20 12:03:27 +0300

    выборка с помощью GROUP BY

--- DEMO.py
+++ DEMO.py
@@ -4,11 +4,16 @@ import sqlite3
 connection = sqlite3.connect ('my_db.db')
 cursor = connection.cursor()
 
-cursor.execute('SELECT username, age FROM users WHERE age > ?', (25,))
+cursor.execute('SELECT age, AVG(age) FROM users GROUP BY age')
 results = cursor.fetchall()
-
 #вывод
 for row in results:
+    print(row)
+
+#фильтр по ср возраст >30
+cursor.execute('SELECT age, AVG(age) FROM Users GROUP BY age HAVING AVG(age) > ?', (30,))
+filtered_results = cursor.fetchall()
+for row in filtered_results:
     print(row)
 
 connection.close()
