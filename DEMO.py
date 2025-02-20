b4a5d44f3244a250b58df4f43e14bd1f5e78e0c5
Postolow Alex <sascha.postoloff@yandex.ru>
2025-02-20 12:04:46 +0300

    выборка с помощью ORDER BY

--- DEMO.py
+++ DEMO.py
@@ -4,16 +4,22 @@ import sqlite3
 connection = sqlite3.connect ('my_db.db')
 cursor = connection.cursor()
 
-cursor.execute('SELECT age, AVG(age) FROM users GROUP BY age')
+##cursor.execute('SELECT age, AVG(age) FROM users GROUP BY age')
+##results = cursor.fetchall()
+###вывод
+##for row in results:
+##    print(row)
+##
+###фильтр по ср возраст >30
+##cursor.execute('SELECT age, AVG(age) FROM Users GROUP BY age HAVING AVG(age) > ?', (30,))
+##filtered_results = cursor.fetchall()
+##for row in filtered_results:
+##    print(row)
+
+cursor.execute('SELECT age, AVG(age) FROM users ORDER BY age DESC')
 results = cursor.fetchall()
 #вывод
 for row in results:
-    print(row)
-
-#фильтр по ср возраст >30
-cursor.execute('SELECT age, AVG(age) FROM Users GROUP BY age HAVING AVG(age) > ?', (30,))
-filtered_results = cursor.fetchall()
-for row in filtered_results:
     print(row)
 
 connection.close()
